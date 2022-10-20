afaf3e2fc92c80b01f6a378d768fad10ca6fa0b5
Ankur Sharma <ankur.101045@gmail.com>
2022-10-20 17:17:27 +0530

    reverted boolean change

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -58,12 +58,12 @@ export class HomeComponent implements OnInit {
     let mainData: any = {files: []};
     const options = { type: "image/gif" };
     this.http.get(url,{ responseType: 'arraybuffer' }).subscribe(async resp=>{
-      const imgFile = new File([resp], `${id}.gif`, options);
-      this.loading = false;
+      const imgFile = new File([resp], `${id}.gif`, options);   
       mainData.files.push(imgFile);
       await navigator.share(
         mainData
       )
+      this.loading = false;
     })
   }
     // this.openSnackBar("gif copied!!");
